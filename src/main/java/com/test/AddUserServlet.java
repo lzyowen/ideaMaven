cc567e216686839e6811cc125abc261b69da175a
lzyowen <liu.zhiyu@aliyun.com>
2019-06-28 17:55:05 +0800

    first commit

---
+++ src/main/java/com/test/AddUserServlet.java
@@ -0,0 +1,21 @@
+package com.test;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AddUserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        String username = request.getParameter("username");
+        System.out.println(username);
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: owen
+  Date: 2019-06-27
+  Time: 17:40
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>maven test</title>
+</head>
+<body>
+    <form method="post" action="addUser">
+        <input type="text" name="username" value="">
+        <input type="submit" name="ok">
+    </form>
+</body>
+</html>
